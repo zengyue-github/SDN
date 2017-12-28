006f7caaae012d9c79c1b6f04308ea26a295b232
zengyue2016 <1477955264@qq.com>
2017-12-28 11:34:43 +0800

    Create ryu.sh

---
+++ INSTALL/ryu.sh
@@ -0,0 +1,13 @@
+sudo apt-get install python-eventlet 
+
+sudo apt-get install python-routes 
+
+sudo apt-get install python-webob 
+
+sudo apt-get install python-paramiko
+
+git clone git://github.com/osrg/ryu.git 
+
+pip install ryu
+
+ryu-manager
